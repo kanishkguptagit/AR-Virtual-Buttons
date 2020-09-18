4b0989d559fec3a590568715fe9055c373405fce
kanishkguptagit <1905902@kiit.ac.in>
2020-09-18 20:08:17 +0530

    Initial commit

---
+++ Assets/scripts/VB2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VB2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public Rigidbody ball;
+    public GameObject vbutton;
+    public int force = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbutton = GameObject.Find("VirtualButton 2");
+        vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ball.AddForce(new Vector3(0, -force, 0));
+        Debug.Log("Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/VB4.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VB4 : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public Rigidbody ball;
+    public GameObject vbutton;
+    public int force = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbutton = GameObject.Find("VirtualButton 4");
+        vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ball.AddForce(new Vector3(-force, 0, 0));
+        Debug.Log("Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
